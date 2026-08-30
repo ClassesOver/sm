15a26e89f70ff7ade7dc5c4754b5d49b4f7f711e
zhaoxiangjun <xiangjun.zhao@hand-china.com>
2026-08-30 10:49:24 +0800

    expose task execution kernel lazily

--- smart_reporting/task_execution/__init__.py
+++ smart_reporting/task_execution/__init__.py
@@ -13,4 +13,19 @@ __all__ = [
     "TaskState",
     "TaskExecutionRepository",
     "TaskScope",
+    "TaskExecutionKernel",
+    "WorkspaceTaskToolkit",
+    "create_files_patch",
 ]
+
+
+def __getattr__(name: str):
+    if name in {"TaskExecutionKernel", "WorkspaceTaskToolkit", "create_files_patch"}:
+        from .execution import TaskExecutionKernel, WorkspaceTaskToolkit, create_files_patch
+
+        return {
+            "TaskExecutionKernel": TaskExecutionKernel,
+            "WorkspaceTaskToolkit": WorkspaceTaskToolkit,
+            "create_files_patch": create_files_patch,
+        }[name]
+    raise AttributeError(name)
